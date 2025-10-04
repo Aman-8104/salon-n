13e1d028f42bf8aae305cac3d098941bae4b2453
Aman-8104 <2023.aman.yadav@ves.ac.in>
2025-10-04 16:10:01 +0530

    Improve server startup log message
    Updated log message for server startup to be more accurate.

--- src/server.js
+++ src/server.js
@@ -59,6 +59,8 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  // The log message is now more accurate for both environments
+  console.log(`Server is running on port ${PORT}`);
 });
